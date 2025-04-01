8e86e82c7a75bb2b5f53edd8f396d94cd783624f
skarsden <zachfrancis98@gmail.com>
2025-04-01 15:09:17 -0400

    Formatted report

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -36,7 +37,7 @@ func main() {
 
 	cfg, err := configure(baseURL, maxConcurrency, maxPages)
 	if err != nil {
-		fmt.Errorf("error - configure: %v", err)
+		fmt.Printf("error - configure: %v", err)
 		return
 	}
 
@@ -46,8 +47,40 @@ func main() {
 	go cfg.crawlPage(baseURL)
 	cfg.wg.Wait()
 
-	for key, value := range cfg.pages {
-		fmt.Printf("%s - %d\n", key, value)
-	}
+	printReport(cfg.pages, baseURL)
 
+}
+
+type Page struct {
+	URL   string
+	Count int
+}
+
+func printReport(pages map[string]int, baseUrl string) {
+	fmt.Printf(`
+==============================
+	REPORT for %s
+==============================
+`, baseUrl)
+
+	sortedPages := sortPages(pages)
+	for _, page := range sortedPages {
+		url := page.URL
+		count := page.Count
+		fmt.Printf("Found %d internal links to %s\n", count, url)
+	}
+}
+
+func sortPages(pages map[string]int) []Page {
+	pagesSlice := []Page{}
+	for url, count := range pages {
+		pagesSlice = append(pagesSlice, Page{URL: url, Count: count})
+	}
+	sort.Slice(pagesSlice, func(i, j int) bool {
+		if pagesSlice[i].Count == pagesSlice[j].Count {
+			return pagesSlice[i].URL < pagesSlice[j].URL
+		}
+		return pagesSlice[i].Count > pagesSlice[j].Count
+	})
+	return pagesSlice
 }
